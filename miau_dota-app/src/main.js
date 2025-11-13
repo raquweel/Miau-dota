0b25536a5c0a16a1c395017cb51958eb69e366db
LuMi-Yo <luismiguelcarvalho65@gmail.com>
2025-11-13 09:39:41 -0300

    Fix: Atualiza a função createpet, adicionando o link e um transforma o id pata1 em uma classe.

--- miau_dota-app/src/main.js
+++ miau_dota-app/src/main.js
@@ -3,12 +3,14 @@ import pets from "./pets.js";
 
 export function createPet(pets) {
   return `
+  <a href="details-pet.html?id=${pets.id}">
     <div>
         <img src="${pets.image}">
         <p class="name">${pets.name}</p>
         <p class="infos-pet">${pets.age} | ${pets.sexo}</p>
-        <img src="./src/imgs/miaudota/pata1.png" alt="pata1" id="pata1">
-    </div>`;
+        <img src="./src/imgs/miaudota/pata1.png" alt="pata1" class="pata1">
+    </div> 
+  </a>`;
 }
 
 export const petcard = document.querySelector(".pets");
