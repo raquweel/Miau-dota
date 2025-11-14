432c53e2083f4b66aa371bd82902299774013e23
LuMi-Yo <luismiguelcarvalho65@gmail.com>
2025-11-14 11:36:35 -0300

    fix: conserta endereço do elemento pata1 nob main.js.

--- miau_dota-app/src/main.js
+++ miau_dota-app/src/main.js
@@ -10,7 +10,7 @@ export function createPet(pets) {
           <img src="${pets.image}">
           <p class="name">${pets.name}</p>
           <p class="infos-pet">${pets.age} | ${pets.sexo}</p>
-          <img src="../src/imgs/miaudota/pata1.png" alt="pata1" class="pata1">
+          <img src="./src/imgs/miaudota/pata1.png" alt="pata1" class="pata1">
       </a>
     </div>` 
   ;
